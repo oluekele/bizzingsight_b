f31ddb327426e88c1008d09acdd315790cde9e91
unknown <ekeleolu1@gmail.com>
2025-10-25 22:45:59 +0100

    render db

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,12 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: nodeEnv === 'production' ? frontendUrl : true,
+    origin:
+      nodeEnv === 'production'
+        ? frontendUrl
+        : nodeEnv === 'development'
+          ? 'http://localhost:3000'
+          : true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
